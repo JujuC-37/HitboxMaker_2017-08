0903460840cb05adc4d54aed26c65559eac91d04
Juju C <juliette.coustard@gmail.com>
2017-09-11 10:20:14 +0200

    POI: build, no modification in src

--- src/buildings.js
+++ src/buildings.js
@@ -2,11 +2,16 @@ import {logosList, buildingsList} from "./data.js";
 
 const $toolsList = document.getElementById('toolsList');
 
-// ----- display -----
+// ----- display list -----
 export function displayBuildingsList() {
     Object.keys(buildingsList).forEach( key => {
         $toolsList.innerHTML += `<div class="selectableTool building" id="${buildingsList[key].id}_toolsList">
                 <img src="${buildingsList[key].logo.src}" alt="${buildingsList[key].logo.alt}" title="${buildingsList[key].logo.title}">
             </div>`
     } );
+}
+
+// ----- Construction -----
+export function constructBuildingOnTile(building) {
+    tile.innerHTML = `<img src="${building.logo.src}" alt="${building.logo.alt}" title="${building.logo.title}">`
 }
